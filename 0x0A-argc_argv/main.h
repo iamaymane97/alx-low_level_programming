63066cad3adee67d23c27b5c30bb81e71af63e67
iamaymane97git config --global user.name iamaymane97 <aymaneelallouch@gmail.com>
2023-10-27 11:11:25 -0700

    0

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+		
+#define MAIN_H
+		
+
+		
+int _putchar(char c);
+		
+int _atoi(char *s);
+		
+
+		
+#endi#ifndef MAIN_H
+		
+#define MAIN_H
+		
+
+		
+int _putchar(char c);
+		
+int _atoi(char *s);
+		
+
+		
+#endiff
