7c44da78458e7879ba592d5cb3b3b2c38ed08e1e
iamaymane97 <aymaneelallouch@gmail.com>
2023-11-16 07:27:48 -0800

    task 6

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,26 +1,16 @@
-#include "lists.h"
+#include<stdio.h>
 
 /**
- * free_list - free single list link
- *
- * @head: pointer to the first node of the list
- *
- * Return: nothing
-*/
-void free_list(list_t *head)
-{
-	list_t *current;
+ * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
+ *   is executed before main()
+ */
+void myStartupFun(void) __attribute__ ((constructor));
 
-	/**
-	 * while current head is not NULL
-	 * set head as the next node and
-	 * free the current node str then
-	 * free current node.
-	*/
-	while ((current = head) != NULL)
-	{
-		head = head->next;
-		free(current->str);
-		free(current);
-	}
+/**
+ * myStartupFun - implementation of myStartupFun
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
